feb6b091ecab287f69d26b0140b92a0d2c8227bd
hamdi <javs_shao@163.com>
2018-12-27 11:35:22 +0800

    设置下载间隔为250ms

--- zhihuSpider/zhihuSpider/settings.py
+++ zhihuSpider/zhihuSpider/settings.py
@@ -88,3 +88,6 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+
+DOWNLOAD_DELAY = 0.25   #设置下载间隔为250ms
